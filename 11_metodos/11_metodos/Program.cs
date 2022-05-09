5c8be5478fff99d97ea517608a9b1c5be546a33a
hernanpm101 <hernanpm101@hotmail.com>
2022-05-09 06:04:27 -0300

    Metodos video 11

---
+++ 11_metodos/11_metodos/Program.cs
@@ -0,0 +1,33 @@
+﻿/*  Este metodo ya sin el void lo remplazamos por return 
+ lo que hace es devolver la suma por medio del return, no importa si por consola lo imprime
+trabajamos por ahora con datos primitivos int, double, float, etc */
+
+
+
+
+class program
+{
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine(sumaNumeros(7, 9));
+
+    }
+
+    static int sumaNumeros(int num1, int num2)    // le dicmos al metodo que es un dato int
+    {
+        return num1 + num2;     // con el return se sale de la ejecucion de un metodo y devuelve
+                                 // el flujo de ejecucion a la llamada
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ 11_metodos2/11_metodos2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+class program
+{
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine(dibideNumeros(7, 9));
+
+    }
+
+    static double dibideNumeros(double num1, int num2)
+    {
+
+        return num1 / num2;
+
+    }
+
+    // static double dibideNumeros(double num1, int num2) => num1 / num2;    otra forma de escribir el metodo anterior
+
+}
