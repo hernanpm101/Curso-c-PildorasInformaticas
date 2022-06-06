b2cd90e3b522e3d7d3ae39da95d8be3933139d15
Hernanpm101 <68437058+hernanpm101@users.noreply.github.com>
2022-06-06 06:41:42 -0300

    Add files via upload

---
+++ 13_metodos_parametros_opcionales/13_metodos_parametros_opcionales/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Al pararnos en "return" con el mouse y hacemos click va a parecer un destornillador a la izquierda,
+ al hacer click nos despliega "acciones rapidas" y podemmos cambiarlo mas eficiente*/
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Suma(7,5.3));
+    }
+
+    private static double Suma(int v1, double v2) => v1 + v2;
+}
+
+/* en el anterior ejemplo, el metodo "Suma" estaba de la siguiente manera:
+ 
+   private static double Suma(int v1, double v2)
+{
+    return v1 + v2;
+}
+
+pero al hacer click sobre return, destornollador, acciones rapidas la cambiamos
+y ahora al tener una sola linea de codigo lo sustituimos para mejor con el simbolo
+"lambda" =>  
+
+*/
+
+
---
+++ 14_condicional_if/14_condicional_if/Program.cs
@@ -0,0 +1,22 @@
+﻿//Condicional If - buleanos - operadorres logicos
+
+
+namespace CondicionalIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            bool haceFrio;
+
+            haceFrio = false;
+
+            Console.WriteLine(!haceFrio);    //invierte su valor, no confundir con != ya que no es lo mismo.
+
+
+
+        }
+    }
+}
